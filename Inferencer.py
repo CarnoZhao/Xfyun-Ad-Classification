7c49e925aea9c72b9f794841611cd9b0b115a4cb
carnozhao <273806108@qq.com>
2021-09-23 16:52:24 +0800

    add final prediction inferencer

---
+++ Inferencer.py
@@ -0,0 +1,26 @@
+import Extractor_Image as ei
+import Extractor_Text as et
+torch = ei.torch
+np = ei.np
+pd = ei.pd
+os = ei.os
+
+if __name__ == "__main__":
+    model_image = ei.ModelPred(tta = False, **ei.args)
+    model_text = et.ModelPred(**et.args)
+
+    ckpt_image = "./logs/pseudo/b0ns_fitter/sorted_all/checkpoints/epoch=22_valid_metric=1.000.ckpt"
+    ckpt_text = "./logs/pseudo/rbt/sorted_all/checkpoints/epoch=29_valid_metric=0.966.ckpt"
+
+    model_image = model_image.load_from_checkpoint(ckpt_image)
+    model_text = model_text.load_from_checkpoint(ckpt_text)
+
+    pred_image = ei.trainer.predict(model_image)
+    pred_image = torch.cat(pred_image).softmax(1).detach().cpu().numpy()
+    pred_text = et.trainer.predict(model_text)
+    pred_text = torch.cat(pred_text).softmax(1).detach().cpu().numpy()
+
+    pred = (0.6 * pred_image + 0.4 * pred_text).argmax(1)
+    image_id = model_image.predict_dataloader().dataset.df.file_name.apply(os.path.basename)
+    sub = pd.DataFrame({"image_id": image_id, "category_id": pred})
+    sub.to_csv("submission.csv", index = False)
